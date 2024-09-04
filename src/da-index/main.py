4ee804476b7fe2633991a8856f5c5f9cdda7634b
HarrisonWilde <harrisondwilde@outlook.com>
2024-09-04 10:29:08 +0100

    Add ruff formatting

--- src/da-index/main.py
+++ src/da-index/main.py
@@ -1,39 +1,48 @@
-from typing import Callable, Any, Union
+import logging
+from typing import Any, Callable, Union
 
 import numpy as np
-from sklearn.neighbors import KernelDensity
-from matplotlib import pyplot as plt
 import pandas as pd
-import logging
+from matplotlib import pyplot as plt
+from sklearn.neighbors import KernelDensity
 
 
 class Core(object):
     @staticmethod
-    def vis_plot(x, x_d, logprob, ylabel='Percentage/PDF', title='', vlines=[], vline_colors=[], vline_labels=[], hist=True,
-                 reverse=False):
+    def vis_plot(
+        x,
+        x_d,
+        logprob,
+        ylabel="Percentage/PDF",
+        title="",
+        vlines=[],
+        vline_colors=[],
+        vline_labels=[],
+        hist=True,
+        reverse=False,
+    ):
         """
         plot probability density function (PDF) and histogram for comparison
         """
         if hist:
-            pd.DataFrame(x).rename(columns={0: 'Histogram'}).plot.hist(bins=20, alpha=0.5, color="goldenrod",
-                                                                       **{"density": True},
-                                                                       figsize=(18, 16)
-                                                                       )
+            pd.DataFrame(x).rename(columns={0: "Histogram"}).plot.hist(
+                bins=20, alpha=0.5, color="goldenrod", **{"density": True}, figsize=(18, 16)
+            )
         else:
             plt.figure(figsize=(8, 2), dpi=100)
-        plt.fill_between(x_d, np.exp(logprob), alpha=0.5, label='Probability Density Function')
+        plt.fill_between(x_d, np.exp(logprob), alpha=0.5, label="Probability Density Function")
         # plt.plot(x, np.full_like(x, -0.005), '|k', markeredgewidth=1)
         for vl, c, l in zip(vlines, vline_colors, vline_labels):
-            plt.axvline(x=vl, color=c, linestyle='--', label=f'{l}:{vl}')
+            plt.axvline(x=vl, color=c, linestyle="--", label=f"{l}:{vl}")
 
         if len(vlines) > 0:
             if not reverse:
-                plt.axvspan(vlines[-1], x_d[-1], facecolor='b', alpha=0.05)
+                plt.axvspan(vlines[-1], x_d[-1], facecolor="b", alpha=0.05)
             else:
-                plt.axvspan(x_d[0], vlines[-1], facecolor='b', alpha=0.05)
+                plt.axvspan(x_d[0], vlines[-1], facecolor="b", alpha=0.05)
 
         plt.legend()
-        plt.ylabel('')
+        plt.ylabel("")
         plt.title(title)
         plt.yticks([])
         # plt.ylim((0, .03))
@@ -44,16 +53,12 @@ class Core(object):
         """
         search for the best bandwith for KDE
         """
-        from sklearn.model_selection import GridSearchCV
-        from sklearn.model_selection import KFold
+        from sklearn.model_selection import GridSearchCV, KFold
 
         bandwidths = np.linspace(0, 1, 20)
-        grid = GridSearchCV(KernelDensity(kernel='gaussian'),
-                            {'bandwidth': bandwidths},
-                            cv=KFold(5)
-                            )
-        grid.fit(x);
-        return grid.best_params_['bandwidth']
+        grid = GridSearchCV(KernelDensity(kernel="gaussian"), {"bandwidth": bandwidths}, cv=KFold(5))
+        grid.fit(x)
+        return grid.best_params_["bandwidth"]
 
     @staticmethod
     def get_transformer(X):
@@ -61,8 +66,8 @@ class Core(object):
         tried for transform input data before KDE
         - didn't work, not in use
         """
-        from sklearn.preprocessing import PowerTransformer
-        from sklearn.preprocessing import Normalizer
+        from sklearn.preprocessing import Normalizer, PowerTransformer
+
         # bc = PowerTransformer(method="box-cox")
         yj = PowerTransformer(method="yeo-johnson")
         X_trans_bc = yj.fit(X)
@@ -80,7 +85,7 @@ class Core(object):
         """
         threshold_index = int((threshold - low_bound) / step)
         if threshold == low_bound + boundary_offset:
-            logging.info(f'adjust boundary {threshold}')
+            logging.info(f"adjust boundary {threshold}")
             threshold_index = int((threshold - low_bound - boundary_offset) / step)
         elif is_discrete:
             # discrete values will lead to PDF shape like pulses,
@@ -101,16 +106,17 @@ class Core(object):
         return first_zero_idx, bins[first_zero_idx]
 
     @staticmethod
-    def kde_estimate(X, bandwidth=1, kernel='gaussian', search_bandwidth=True):
+    def kde_estimate(X, bandwidth=1, kernel="gaussian", search_bandwidth=True):
         """
         Kernel density estimation to get probability
         """
         # KDE estimate
         from sklearn.neighbors import KernelDensity
+
         best_bw = bandwidth
         if search_bandwidth:
             best_bw = Core.grid_search_bandwith(X)
-            logging.info(f'learned best bandwidth {best_bw}')
+            logging.info(f"learned best bandwidth {best_bw}")
 
         kde = KernelDensity(bandwidth=best_bw, kernel=kernel)
         kde.fit(X)
@@ -118,13 +124,22 @@ class Core(object):
         return kde, best_bw
 
     @staticmethod
-    def deterioration_index(X, low_bound, up_bound, threshold,
-                            n_samples=10000, plot_title='',
-                            is_discrete=False, prev_discrete_value_offset=1,
-                            weight_sum_steps=10, reverse=False,
-                            bandwidth=1, kernel='gaussian', search_bandwidth=True,
-                            do_plot=True
-                            ):
+    def deterioration_index(
+        X,
+        low_bound,
+        up_bound,
+        threshold,
+        n_samples=10000,
+        plot_title="",
+        is_discrete=False,
+        prev_discrete_value_offset=1,
+        weight_sum_steps=10,
+        reverse=False,
+        bandwidth=1,
+        kernel="gaussian",
+        search_bandwidth=True,
+        do_plot=True,
+    ):
         """
         obtain deterioration index
         X - the random sample of measurements
@@ -144,22 +159,23 @@ class Core(object):
         """
 
         # estimate density function
-        kde, fitted_bandwith = Core.kde_estimate(X, bandwidth=bandwidth, kernel=kernel,
-                                                 search_bandwidth=search_bandwidth)
+        kde, fitted_bandwith = Core.kde_estimate(
+            X, bandwidth=bandwidth, kernel=kernel, search_bandwidth=search_bandwidth
+        )
 
         # detect pulse like PDF
-        if fitted_bandwith < .1:
+        if fitted_bandwith < 0.1:
             # force is_discrete because the bestfitted_bandwith_bw would lead to one
-            logging.info(f'FORCED to be discrete because the bandwidth is {fitted_bandwith}')
+            logging.info(f"FORCED to be discrete because the bandwidth is {fitted_bandwith}")
             is_discrete = True
-        elif fitted_bandwith > .7:
+        elif fitted_bandwith > 0.7:
             # force is_discrete to be false because the fitted_bandwith would lead to one
-            logging.info(f'FORCED to be NOT discrete because the bandwidth is {fitted_bandwith}')
+            logging.info(f"FORCED to be NOT discrete because the bandwidth is {fitted_bandwith}")
             is_discrete = False
 
         # automatically adjust on boundaries
         _, adjusted_min = Core.search_for_zero_mass_index(kde, low_bound)
-        logging.info(f'adjusted min val {adjusted_min}')
+        logging.info(f"adjusted min val {adjusted_min}")
         boundary_offset = low_bound - adjusted_min
         orig_low_bound = low_bound
         low_bound -= boundary_offset
@@ -172,12 +188,25 @@ class Core(object):
         prob = np.exp(kd_vals) * step_width  # get the approximate prob at each point using the integral of the PDF
 
         if do_plot:
-            tidx = Core.get_threshold_index(threshold, low_bound, is_discrete, prev_discrete_value_offset, step_width,
-                                            boundary_offset=boundary_offset)
-            Core.vis_plot(X, bins, kd_vals, title=plot_title,
-                          vlines=[threshold, round(tidx * step_width + low_bound, 2)], vline_colors=['r', 'b'],
-                          vline_labels=['Threshold', 'Boudary-adjusted'],
-                          hist=False, reverse=reverse)
+            tidx = Core.get_threshold_index(
+                threshold,
+                low_bound,
+                is_discrete,
+                prev_discrete_value_offset,
+                step_width,
+                boundary_offset=boundary_offset,
+            )
+            Core.vis_plot(
+                X,
+                bins,
+                kd_vals,
+                title=plot_title,
+                vlines=[threshold, round(tidx * step_width + low_bound, 2)],
+                vline_colors=["r", "b"],
+                vline_labels=["Threshold", "Boudary-adjusted"],
+                hist=False,
+                reverse=reverse,
+            )
 
         # severity quantification
 
@@ -188,17 +217,38 @@ class Core(object):
             s = max(threshold, low_bound)
             e = up_bound
         # 1. binary like multimorbidity num > 3, yes or no
-        sq1 = Core.stepped_severity(prob, s, e, 1, low_bound, step_width, is_discrete, boundary_offset,
-                                    prev_val_offset=prev_discrete_value_offset, reverse=reverse)
+        sq1 = Core.stepped_severity(
+            prob,
+            s,
+            e,
+            1,
+            low_bound,
+            step_width,
+            is_discrete,
+            boundary_offset,
+            prev_val_offset=prev_discrete_value_offset,
+            reverse=reverse,
+        )
         # 2. stepped quantification that considers higher/lower the value, more severe the patients are
-        sqs = Core.stepped_severity(prob, s, e, weight_sum_steps, low_bound, step_width, is_discrete, boundary_offset,
-                                    prev_val_offset=prev_discrete_value_offset, reverse=reverse)
+        sqs = Core.stepped_severity(
+            prob,
+            s,
+            e,
+            weight_sum_steps,
+            low_bound,
+            step_width,
+            is_discrete,
+            boundary_offset,
+            prev_val_offset=prev_discrete_value_offset,
+            reverse=reverse,
+        )
 
-        return {'overall-prob': round(prob.sum(), 4), 'one-step': round(sq1, 4), 'k-step': round(sqs, 6), '|X|': len(X)}
+        return {"overall-prob": round(prob.sum(), 4), "one-step": round(sq1, 4), "k-step": round(sqs, 6), "|X|": len(X)}
 
     @staticmethod
-    def stepped_severity(probs, s, e, steps, low_bound, step_width, is_discrete, boundary_offset, prev_val_offset=1,
-                         reverse=False) -> float:
+    def stepped_severity(
+        probs, s, e, steps, low_bound, step_width, is_discrete, boundary_offset, prev_val_offset=1, reverse=False
+    ) -> float:
         """
         To quantify severity by considering higher values as more severe.
         This is done by weighted sum by integrating probs from a threshold - s
@@ -210,17 +260,22 @@ class Core(object):
         for i, t in enumerate(bins):
             idx1 = Core.get_threshold_index(t, low_bound, is_discrete, prev_val_offset, step_width, boundary_offset)
             if i < len(bins) - 1:
-                idx2 = Core.get_threshold_index(bins[i + 1], low_bound, is_discrete, prev_val_offset, step_width,
-                                                boundary_offset)
+                idx2 = Core.get_threshold_index(
+                    bins[i + 1], low_bound, is_discrete, prev_val_offset, step_width, boundary_offset
+                )
                 bin_probs.append(probs[idx1:idx2].sum())
             else:
-                bin_probs.append(probs[idx1:].sum()) if not reverse else bin_probs.append(
-                    probs[idx1:Core.get_threshold_index(e,
-                                                        low_bound,
-                                                        is_discrete,
-                                                        prev_val_offset,
-                                                        step_width,
-                                                        boundary_offset)].sum())
+                (
+                    bin_probs.append(probs[idx1:].sum())
+                    if not reverse
+                    else bin_probs.append(
+                        probs[
+                            idx1 : Core.get_threshold_index(
+                                e, low_bound, is_discrete, prev_val_offset, step_width, boundary_offset
+                            )
+                        ].sum()
+                    )
+                )
         s = 0
 
         # weight functions
@@ -241,7 +296,7 @@ class Core(object):
         """
         quantify inequality
         """
-        return di1['k-step'] / di2['k-step'] - 1
+        return di1["k-step"] / di2["k-step"] - 1
 
 
 class Util(object):
@@ -255,10 +310,21 @@ class Util(object):
         return X
 
     @staticmethod
-    def compare_two_groups(df1, df2, feature, cohort_name1, cohort_name2, di_label, threshold,
-                           bandwidth=1, is_discrete=False, search_bandwidth=True, do_plot=True,
-                           feature_gen_fun=None, reverse=False
-                           ):
+    def compare_two_groups(
+        df1,
+        df2,
+        feature,
+        cohort_name1,
+        cohort_name2,
+        di_label,
+        threshold,
+        bandwidth=1,
+        is_discrete=False,
+        search_bandwidth=True,
+        do_plot=True,
+        feature_gen_fun=None,
+        reverse=False,
+    ):
         """
         # obtain the inequality quantification
         df1 - data frame of the first group
@@ -283,14 +349,30 @@ class Util(object):
         if type(threshold) is list:
             threshold1 = threshold[0]
             threshold2 = threshold[1]
-        c1_di = Core.deterioration_index(X1, min_v, max_v, reverse=reverse, threshold=threshold1, bandwidth=bandwidth,
-                                         is_discrete=is_discrete,
-                                         plot_title=f'{cohort_name1}| {di_label}', search_bandwidth=search_bandwidth,
-                                         do_plot=do_plot)
-        c2_di = Core.deterioration_index(X2, min_v, max_v, reverse=reverse, threshold=threshold2, bandwidth=bandwidth,
-                                         is_discrete=is_discrete,
-                                         plot_title=f'{cohort_name2}| {di_label}', search_bandwidth=search_bandwidth,
-                                         do_plot=do_plot)
+        c1_di = Core.deterioration_index(
+            X1,
+            min_v,
+            max_v,
+            reverse=reverse,
+            threshold=threshold1,
+            bandwidth=bandwidth,
+            is_discrete=is_discrete,
+            plot_title=f"{cohort_name1}| {di_label}",
+            search_bandwidth=search_bandwidth,
+            do_plot=do_plot,
+        )
+        c2_di = Core.deterioration_index(
+            X2,
+            min_v,
+            max_v,
+            reverse=reverse,
+            threshold=threshold2,
+            bandwidth=bandwidth,
+            is_discrete=is_discrete,
+            plot_title=f"{cohort_name2}| {di_label}",
+            search_bandwidth=search_bandwidth,
+            do_plot=do_plot,
+        )
         ineq = Core.db_ineq(c1_di, c2_di)
         # print(f'{cohort_name1} vs {cohort_name2} inequality on {di_label} is {ineq:.2%}')
         return c1_di, c2_di, ineq
@@ -329,8 +411,8 @@ class Util(object):
         """
         w_data = data[np.where(data[:, 1] > 0)][:, [0, 2, 1]]
         a, decision_area = Util.area(w_data)
-        plt.plot(w_data[:, 0], w_data[:, 1], '-')
-        plt.plot(w_data[:, 0], w_data[:, 1], 'o', label=label)
+        plt.plot(w_data[:, 0], w_data[:, 1], "-")
+        plt.plot(w_data[:, 0], w_data[:, 1], "o", label=label)
         return a, decision_area, w_data
 
     @staticmethod
@@ -338,11 +420,11 @@ class Util(object):
         """
         do DA curve visualisation
         """
-        if 'style' in config:
-            plt.style.use(config['style'])
-        font_size = config['font_size'] if 'font_size' in config else 12
-        if 'fig_size' in config:
-            fig = plt.figure(figsize=config['fig_size'], dpi=200)
+        if "style" in config:
+            plt.style.use(config["style"])
+        font_size = config["font_size"] if "font_size" in config else 12
+        if "fig_size" in config:
+            fig = plt.figure(figsize=config["fig_size"], dpi=200)
 
         # do some clearning: remove those empty points
         d1 = np.delete(d1, np.where(d1[:, 1] == 0), axis=0)
@@ -366,15 +448,15 @@ class Util(object):
         # generate output
         # print('{0}\t{1:.2%}\t{2:.2%}\t{3:.2%}'.format(deterioration, white_d_ratio, non_white_d_ratio,
         #                                      (non_white_d_ratio - white_d_ratio)/white_d_ratio))
-        print('AUC\t{0:.6f}\t{1:.6f}\t{2:.2%}'.format(a1, a2, (a2 - a1) / a1))
-        print('Decision AUC\t{0:.6f}\t{1:.6f}\t{2:.2%}'.format(da1, da2, (da2 - da1) / da1))
+        print("AUC\t{0:.6f}\t{1:.6f}\t{2:.2%}".format(a1, a2, (a2 - a1) / a1))
+        print("Decision AUC\t{0:.6f}\t{1:.6f}\t{2:.2%}".format(da1, da2, (da2 - da1) / da1))
 
         # figure finishing up
         plt.xlabel(allocation_label, fontsize=font_size)
         plt.ylabel(deterioration_label, fontsize=font_size)
 
         # plot decision region
-        plt.plot([0.5, 0.5], [0, 1], '--', lw=.8, color='g')
-        plt.axvspan(0.5, 1, facecolor='b', alpha=0.1)
+        plt.plot([0.5, 0.5], [0, 1], "--", lw=0.8, color="g")
+        plt.axvspan(0.5, 1, facecolor="b", alpha=0.1)
 
-        plt.legend(fontsize=font_size, loc='best')
+        plt.legend(fontsize=font_size, loc="best")
